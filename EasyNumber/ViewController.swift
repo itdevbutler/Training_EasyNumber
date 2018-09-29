42b187d5f297d822baee7d76711fe396f72e8c1a
itdev <it.dev@digitalbutler.co.th>
2018-09-29 15:57:50 +0700

    update design and function

--- EasyNumber/ViewController.swift
+++ EasyNumber/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var numberLable: UILabel!
+    
+    @IBAction func increaseNumber(_ sender: Any) {
+        
+        showButton(nameButton: "inCreaseButton")
+        
+    }
+    
+    @IBAction func startNumber(_ sender: Any) {
+        showButton(nameButton: "showButton")
+    }
+    
+    @IBAction func decreaseNumber(_ sender: Any) {
+        showButton(nameButton: "deCreaseButton")
+    }
+    
+    func showButton(nameButton: String) -> Void {
+        print("You Click ==> \(nameButton)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
